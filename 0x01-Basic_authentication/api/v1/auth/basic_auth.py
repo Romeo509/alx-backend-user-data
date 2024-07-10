95ce631e00c79384fe6cd8dda3b8b1c4e890727c
Romeo509 <romeoameku503@gmail.com>
2024-07-10 02:02:41 +0000

    basic auth now inherits from auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -79,3 +79,23 @@ class BasicAuth(Auth):
             return user
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the User instance for a request using Basic Authentication
+        """
+        if request is None:
+            return None
+
+        auth_header = self.authorization_header(request)
+        base64_auth_header = (
+            self.extract_base64_authorization_header(auth_header)
+        )
+        decoded_auth_header = (
+            self.decode_base64_authorization_header(base64_auth_header)
+        )
+        user_email, user_pwd = (
+            self.extract_user_credentials(decoded_auth_header)
+        )
+
+        return self.user_object_from_credentials(user_email, user_pwd)
