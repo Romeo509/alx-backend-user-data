3f58ecdbd9f5afc63768b1ab88b8d34d8de117fa
Romeo509 <romeoameku503@gmail.com>
2024-07-10 01:09:12 +0000

    fisxed pycodestyle error for app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,28 +16,34 @@ if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request():
     if auth is None:
         return
-   
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+    excluded_paths = (
+        ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    )
     if not auth.require_auth(request.path, excluded_paths):
         return
-   
+
     if auth.authorization_header(request) is None:
         abort(401)
-   
+
     if auth.current_user(request) is None:
         abort(403)
+
 
 @app.errorhandler(401)
 def unauthorized(error):
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error):
     return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
