843106c5fb6c29ed61406380283fed22f5fd9bb7
Romeo509 <romeoameku503@gmail.com>
2024-07-10 01:40:01 +0000

    basic auth now inherits from auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth module for handling basic authentication
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -23,3 +24,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Decodes the Base64 part of the Authorization header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
