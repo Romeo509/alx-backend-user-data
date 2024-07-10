ff3eaadc21491860e3ea6e5641b57aa1400e4f7b
Romeo509 <romeoameku503@gmail.com>
2024-07-10 01:04:25 +0000

    fisxed pycodestyle error for auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ from flask import request
 
 
 class Auth:
+    """documentation"""
     def authorization_header(self, request=None) -> str:
         """ Returns the Authorization header value from the request """
         if request is None or 'Authorization' not in request.headers:
@@ -13,14 +14,17 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: list) -> bool:
         """ Checks if authentication is required for a given path """
         if path is None:
+            """documentation"""
             return True
 
         if not excluded_paths:
+            """documentation"""
             return True
 
         normalized_path = path.rstrip('/')
 
         for ex_path in excluded_paths:
+            """documentation"""
             normalized_ex_path = ex_path.rstrip('/')
             if normalized_path == normalized_ex_path:
                 return False
