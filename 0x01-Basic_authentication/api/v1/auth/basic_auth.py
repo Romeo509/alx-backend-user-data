1c7c17ffdbd0aec990c6e7485c9bd54e14fb1148
Romeo509 <romeoameku503@gmail.com>
2024-07-10 01:32:30 +0000

    basic auth now inherits from auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+"""
+BasicAuth module for handling basic authentication
+"""
 
 from api.v1.auth.auth import Auth
+
 
 class BasicAuth(Auth):
     """ BasicAuth class for handling basic authentication """
