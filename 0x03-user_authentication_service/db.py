e1c8be8ca57e2bc9298333716199f2a036d98420
Romeo509 <romeoameku503@gmail.com>
2024-07-17 19:00:50 +0000

    Completed the db task

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -17,9 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine(
-            "sqlite:///a.db", echo=False
-        )  # Set echo to False
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -47,3 +47,24 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments
+
+        Args:
+            kwargs: Arbitrary keyword arguments to filter the user by.
+
+        Returns:
+            User: The first user that matches the filter criteria.
+
+        Raises:
+            NoResultFound: If no user is found.
+            InvalidRequestError: If the query is invalid.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the given criteria")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments")
