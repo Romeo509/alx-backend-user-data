c2ad7d7c1d7813a5dcd1833e9dccf5b4cc2ccf41
Romeo509 <romeoameku503@gmail.com>
2024-07-17 22:16:31 +0000

    Completed the db task

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password string with bcrypt and returns the salted hash.
+
+    Args:
+        password (str): The password to be hashed.
+
+    Returns:
+        bytes: The salted hash of the password.
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt)
+    return hashed
