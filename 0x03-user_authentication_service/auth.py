724fc9bf6b6334f4c909f8ccdea8ad8c87e973f8
Romeo509 <romeoameku503@gmail.com>
2024-07-17 23:27:48 +0000

    Asdd user registration endpoint

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,47 +5,34 @@ Auth module
 import bcrypt
 from db import DB
 from user import User
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
-    """
+    """Auth class to interact with the authentication database."""
 
     def __init__(self):
         self._db = DB()
 
-    def _hash_password(self, password: str) -> bytes:
-        """Hashes a password string with bcrypt and returns the salted hash.
-
-        Args:
-            password (str): The password to be hashed.
-
-        Returns:
-            bytes: The salted hash of the password.
-        """
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(password.encode(), salt)
-        return hashed
-
     def register_user(self, email: str, password: str) -> User:
-        """Registers a user with the given email and password.
-
-        Args:
-            email (str): The email of the user.
-            password (str): The password of the user.
-
-        Returns:
-            User: The registered User object.
-
-        Raises:
-            ValueError: If a user with the given email already exists.
-        """
         try:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+        except NoResultFound:
+            return False
+        return False
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt."""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
