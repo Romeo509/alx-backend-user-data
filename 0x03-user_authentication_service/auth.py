febad9fc9fc505b07922418fe672f2784931a63b
Romeo509 <romeoameku503@gmail.com>
2024-07-18 02:20:58 +0000

    Asdd user registration endpoint

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,6 +33,16 @@ class Auth:
             return False
         return False
 
+    def create_session(self, email: str) -> str:
+        """Create a session ID for the user with the given email."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt."""
