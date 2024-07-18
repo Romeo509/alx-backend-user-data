5ba4a257e63eeda462975187e74c922b6b6837a2
Romeo509 <romeoameku503@gmail.com>
2024-07-18 13:27:41 +0000

    implement a get_user_session_from_id method in Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Optional
 
 
 class Auth:
@@ -33,13 +34,29 @@ class Auth:
             return False
         return False
 
-    def create_session(self, email: str) -> str:
+    def create_session(self, email: str) -> Optional[str]:
         """Create a session ID for the user with the given email."""
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(
+        self, session_id: Optional[str]
+    ) -> Optional[User]:
+        """
+        Takes a single session_id string argument and
+        returns the corresponding User or None.
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
 
