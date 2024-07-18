a5f8361de70691832f5839c573fe529f54441da2
Romeo509 <romeoameku503@gmail.com>
2024-07-18 15:55:09 +0000

    implement a destroy_session function in auth class

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,5 +51,16 @@ def logout():
     return redirect(url_for('home'))
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    session_id = request.cookies.get("session_id")
+    if session_id is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
